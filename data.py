f28b7c6425969053e332c99a5b13563857ad198f
Sharon Lee <sharonshlee@icloud.com>
2023-07-29 15:29:43 -0500

    initial commits and search flights by ID

---
+++ data.py
@@ -0,0 +1,52 @@
+import sqlalchemy
+from sqlalchemy import create_engine
+
+QUERY_FLIGHT_BY_ID = "SELECT flights.*, airlines.airline, flights.ID as FLIGHT_ID, flights.DEPARTURE_DELAY as DELAY " \
+                     "FROM flights " \
+                     "JOIN airlines " \
+                     "ON flights.airline = airlines.id " \
+                     "WHERE flights.ID = :id"
+
+
+class FlightData:
+    """
+    The FlightData class is a Data Access Layer (DAL) object that provides an
+    interface to the flight data in the SQLITE database. When the object is created,
+    the class forms connection to the sqlite database file, which remains active
+    until the object is destroyed.
+    """
+
+    def __init__(self, db_uri):
+        """
+        Initialize a new engine using the given database URI
+        """
+        self._engine = create_engine(db_uri)
+
+    def _execute_query(self, query, params):
+        """
+        Execute an SQL query with the params provided in a dictionary,
+        and returns a list of records (dictionary-like objects).
+        If an exception was raised, print the error, and return an empty list.
+        """
+        sql_query = sqlalchemy.text(query)
+        try:
+            with self._engine.connect() as connection:
+                # run query
+                return connection.execute(sql_query, params).fetchall()
+        except Exception as err:
+            print(err)
+            return []
+
+    def get_flight_by_id(self, flight_id):
+        """
+        Searches for flight details using flight ID.
+        If the flight was found, returns a list with a single record.
+        """
+        params = {'id': flight_id}
+        return self._execute_query(QUERY_FLIGHT_BY_ID, params)
+
+    def __del__(self):
+        """
+        Closes the connection to the databse when the object is about to be destroyed
+        """
+        self._engine.dispose()
